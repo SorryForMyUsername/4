95fd65734e2b880df7d6f2fe5f1d6a81258579e9
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2024-10-15 10:52:19 +0300

    Add files via upload

---
+++ Компьютерный практикум №4/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Компьютерный_практикум__4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1
+            //Console.Write("Введите целое число: ");
+            //int number = int.Parse(Console.ReadLine());
+            //if (number > 0)
+            //{
+            //    ++number;
+            //}
+            //else
+            //{
+            //    number -= 2;
+            //}
+            //Console.WriteLine(number);
+
+            //Task 2
+            //Console.Write("Введите целое число: ");
+            //int number = int.Parse(Console.ReadLine());
+            //if (number > 0)
+            //{
+            //    ++number;
+            //}
+            //Console.WriteLine(number);
+
+            //Task 3
+            //Console.Write("Введите целое число: ");
+            //int number = int.Parse(Console.ReadLine());
+            //if (number > 0)
+            //{
+            //    ++number;
+            //}
+            //else if (number < 0)
+            //{
+            //    number -= 2;
+            //}
+            //else
+            //{
+            //    number = 10;
+            //}
+            //Console.WriteLine(number);
+
+            //Task 4
+            //Console.Write("Введите целое число: ");
+            //int number = int.Parse(Console.ReadLine());
+            //if (number % 2 == 0)
+            //{
+            //    Console.WriteLine($"Число {number} - чётное");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Число {number} - нечётное");
+            //}
+
+            //Task 5
+            //Console.Write("Введите целое число: ");
+            //int number = int.Parse(Console.ReadLine());
+            //if (number > 0)
+            //{
+            //    Console.WriteLine($"Число {number} - положительное");
+            //}
+            //else if (number < 0)
+            //{
+            //    Console.WriteLine($"Число {number} - отрицательное");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Число {number} - нулевое");
+            //}
+
+            //Task 6
+            //Console.Write("Введите цену ноутбука: ");
+            //int price = Convert.ToInt32(Console.ReadLine());
+            //if (price >= 20000 && price <= 30000)
+            //{
+            //    Console.WriteLine("Цена ноутбука входит в диапазон от 20 до 30 тыс. рублей.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Цена ноутбука не входит в диапазон от 20 до 30 тыс. рублей.");
+            //}
+
+            //Task 7
+            //Console.Write("Введите текущий год: ");
+            //int year = Convert.ToInt32(Console.ReadLine());
+            //if (year % 4 == 0)
+            //{
+            //    Console.WriteLine("Текущий год является высокосным");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Текущий год не является высокосным");
+            //}
+
+            Console.ReadKey();
+        }
+    }
+}
